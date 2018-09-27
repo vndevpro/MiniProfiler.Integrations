83b503f441d0228c2936d4a5bf55b8fd297966c7
juanonsoftware <juanonsoftware@github.com>
2018-09-27 11:56:06 +0700

    Added MiniProfiler.Integrations.TestAppCore

---
+++ MiniProfiler.Integrations.TestAppCore/Program.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using System;
+
+namespace MiniProfiler.Integrations.TestAppCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            const string connectionString = @"Server=.\SqlExpress;Integrated Security=True;";
+
+            var profiler = CustomDbProfiler.Current;
+            using (var dbConnection = ProfiledDbConnectionFactory.New(new SqlServerDbConnectionFactory(connectionString), profiler))
+            {
+                dbConnection.Execute("SELECT GETDATE() as ServerTime");
+            }
+
+            var commands = profiler.GetCommands();            
+        }
+    }
+}
